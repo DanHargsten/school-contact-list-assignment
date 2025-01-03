9eb21df3c6986121e257d8e51e1838134c27dd3c
Dan Hargsten <dan.hargsten@gmail.com>
2025-01-03 14:20:47 +0100

    Before branch

--- Presentation.ConsoleApp/Dialogs/menuDialog.cs
+++ Presentation.ConsoleApp/Dialogs/menuDialog.cs
@@ -60,39 +60,89 @@ public class menuDialog
 
 
 
-    // Reducera upprepning av kod genom att skapa en hjälpmetod för användarens input
+    // Reducera upprepning av kod genom att skapa en hjälpmetod för användarens input.
     private string GetInput(string promt)
     {
         Console.Write(promt);
-        string input = Console.ReadLine()!;
+        string? input = Console.ReadLine();
         while (string.IsNullOrWhiteSpace(promt))
         {
             Console.WriteLine("Input can't be empty. Please try again.");
             Console.Write(promt);
             input = Console.ReadLine()!;
         }
-        return input;
+        return input!;
     }
 
 
 
+    // Väldigt simpel validering av email.
+    private string GetValidatedEmail()
+    {
+        string email = GetInput("Email: ");
+        while (!email.Contains("@") || !email.Contains("."))
+        {
+            Console.WriteLine("Invalid email format. Please try again");
+            email = GetInput("Email: ");
+        }
+        return email;
+    }
 
+
+
+    // Visar initialt en kompakt lista som enbart inehåller för- och efternamn, samt ID.
+    // Användaren kan sen välja en kontakt via ett index för att se fullständing information.
     public void ShowAllContacts(ContactService contactService)
     {
-        var contacts = contactService.GetAllContacts();
-        if (contacts.Count != 0)
+        while (true)
         {
-            for (int i = 0; i < contacts.Count; i++)
+            Console.Clear();
+
+            var contacts = _contactService.GetAllContacts();
+            if (contacts.Count != 0)
             {
-                Console.WriteLine($"{i + 1}. {contacts[i].CompactContact()}");
+                // Skriver ut en kompakt lista
+                for (int i = 0; i < contacts.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {contacts[i].CompactContact()}");
+                }
+
+                Console.WriteLine("\nEnter the number of a contact to view details, or press Enter to return.");
+                string input = Console.ReadLine()!;
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    OutputMessage($"Invalid input. Please enter a number between 1 and {contacts.Count}.\nPress any key to return to the contact list.");
+                    Console.ReadKey();
+                    return;
+                }
+
+                // Skriver ut detaljerad lista
+                if (int.TryParse(input, out int index) && index > 0 && index <= contacts.Count)
+                {
+                    Console.Clear();
+                    Console.WriteLine(contacts[index - 1].DetailedContact());
+                    Console.WriteLine("\nPress any key to return to the contact list.");
+                    Console.ReadKey();
+                }
+
+                else
+                {
+                    OutputMessage($"Invalid input. Please enter a number between 1 and {contacts.Count}.\n\nPress any key to return to the contact list.");
+                    Console.ReadKey();
+                }
             }
-        }
-        else
-        {
-            OutputMessage("No existing contacts");
+
+            else
+            {
+                OutputMessage("No existing contacts");
+                return;
+            }
         }
     }
     
+
+    // Lägg till kontakt
     public void AddContact(ContactService contactService)
     {
         Console.Clear();
@@ -100,7 +150,7 @@ public class menuDialog
 
         string firstName = GetInput("First Name: ");
         string lastName = GetInput("Last Name: ");
-        string email = GetInput("Email: ");
+        string email = GetValidatedEmail();
         string phone = GetInput("Phone number: ");
         string address = GetInput("Address: ");
         string postalCode = GetInput("Postal Code: ");
@@ -119,7 +169,9 @@ public class menuDialog
         };
 
         contactService.AddContact(newContact);
-        Console.WriteLine($"Contact added successfully\n{newContact.CompactContact()}");
+        Console.Clear();
+        Console.WriteLine($"Contact added successfully!\n{newContact.CompactContact()}");
+        Console.WriteLine("\nPress any key to continue.");
     }
 
 
@@ -127,7 +179,6 @@ public class menuDialog
     public void OutputMessage(string message)
     {
         Console.Clear();
-        Console.WriteLine("test");
         Console.WriteLine(message);
     }
 }
